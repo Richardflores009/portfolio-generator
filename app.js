d46ba24ca95f35361ab9972704fcdb28f7d0f51a
Richy Rich <richardflores009@gmail.com>
2020-09-21 17:34:33 -0700

    added npm and used a new style of promise

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer')
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers))
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
